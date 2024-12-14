2a9a45e5e6eb042ad51e821241f72bad5b6d3b13
Leonardo David <rbdivadoel@gmail.com>
2024-12-14 15:58:58 -0300

    feat: store errors

--- store/src/main/java/imd/ufrn/store/service/ProductService.java
+++ store/src/main/java/imd/ufrn/store/service/ProductService.java
@@ -1,7 +1,10 @@
 package imd.ufrn.store.service;
 
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 import org.springframework.stereotype.Service;
 
@@ -18,11 +21,23 @@ public class ProductService {
     private static final double FREQUENCY = 0.6;
     private static final double BASE = 5.0;
 
+    private static Random random = new Random();
+    private static int timeMinOmission = 10;
+
+    private Instant lastError = Instant.MIN;
+    private Duration errDurationInMin = Duration.ofSeconds(5);
+
     public Product getProduct(Long id) {
+        simulateProductOmission();
+
         return generateDeterministicProductById(id);
     }
 
     public SellResponse sellProduct(Long id) {
+        if (simulateSellError()) {
+            throw new Error();
+        }
+
         Long transactionId = nextTransactionId;
         nextTransactionId++;
         soldProductsLog.add(new Transaction(transactionId, id));
@@ -34,6 +49,54 @@ public class ProductService {
         String name = "productName " + id.toString();
 
         return new Product(id, name, value);
+    }
+
+    private void simulateProductOmission() {
+        int chance = random.nextInt(10);
+        if (chance <= 1) {
+            sleepMinutes(timeMinOmission);
+        }
+    }
+
+    private Boolean simulateSellError() {
+        if (isInErrorState()) {
+            return true;
+        }
+
+        int chance = random.nextInt(10);
+        if (chance == 0) {
+            setStartErrorState();
+            return true;
+        }
+
+        return false;
+    }
+
+    private Boolean isInErrorState() {
+        Instant now = Instant.now();
+        Duration timeSinceLastError = Duration.between(lastError, now);
+
+        if (timeSinceLastError.compareTo(errDurationInMin) <= 0) {
+            return true;
+        } else {
+            return false;
+        }
+
+    }
+
+    private void setStartErrorState() {
+        lastError = Instant.now();
+    }
+
+    private void sleepMinutes(int minutes) {
+        int counter = 0;
+        while (counter < minutes) {
+            try {
+                Thread.sleep(60000);
+            } catch (Exception e) {
+            }
+            counter++;
+        }
     }
 
 }
