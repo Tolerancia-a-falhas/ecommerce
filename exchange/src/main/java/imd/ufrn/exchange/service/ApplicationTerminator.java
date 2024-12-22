14ff872353acfed1c797860bbf5e67e7a74021e2
Leonardo David <rbdivadoel@gmail.com>
2024-12-22 16:49:47 -0300

    feat: exchange fast application terminator

--- exchange/src/main/java/imd/ufrn/exchange/service/ApplicationTerminator.java
+++ exchange/src/main/java/imd/ufrn/exchange/service/ApplicationTerminator.java
@@ -1,7 +1,6 @@
 package imd.ufrn.exchange.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.SpringApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
@@ -12,7 +11,7 @@ public class ApplicationTerminator {
     private ApplicationContext context;
 
     public void stopApplication() {
-        SpringApplication.exit(context, () -> 0);
+        // SpringApplication.exit(context, () -> 0);
         System.exit(0);
     }
 }
